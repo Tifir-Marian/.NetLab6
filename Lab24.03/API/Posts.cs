53a6fa86f8e2a981efde67bf4e21fd2297b50cf6
Tifir <maria_vladut52@yahoo.com>
2020-04-01 14:33:05 +0300

    laborator

---
+++ Lab24.03/API/Posts.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab24._03.API
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    public partial class Posts
+    {
+        
+    }
+}
